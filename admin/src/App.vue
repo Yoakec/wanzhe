2a5ba7d649d37cbb6404b300eee6406f4e73911c
Yoakec <1602491057@qq.com>
2022-03-30 15:25:02 +0800

    initdemo

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -5,17 +5,14 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+    padding: 0;
+    margin: 0;
 }
 </style>
---
+++ admin/src/rounter.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+// 1. 定义路由组件.
+// 也可以从其他文件导入
+import Main from './components/main.vue'
+
+// 2. 定义一些路由
+// 每个路由都需要映射到一个组件。
+// 我们后面再讨论嵌套路由。
+const routes = [
+  { path: '/', component: Main }
+]
+
+// 3. 创建路由实例并传递 `routes` 配置
+// 你可以在这里输入更多的配置，但我们在这里
+// 暂时保持简单
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+  })
+
+
+export default router;
